03335b228a580f457ae96a3b3b0f24b7ef9cc81d
alaa1s <aalaa.alsaheel@gmail.com>
2020-11-28 23:37:49 +0300

    AOS

--- Code.php
+++ Code.php
@@ -10,6 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="main.css">
     <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <script src="script.js"></script>
   </head>
   <body>
@@ -136,6 +137,10 @@ if (isset($_POST['name'])){
 }
  ?>
 </section>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
   </body>
 
   <body>
